b9423c3668e8e37f0c17eb41fa9497aaadd86705
sardar uzair <sardaruzair123456789@gmail.com>
2022-12-15 02:58:02 +0500

    first day django

--- myProject/home/views.py
+++ myProject/home/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    # return HttpResponse('<h1><u>Hello World from sardar uzair</u></h1>') ia used to return the string
+    contex = {
+        'name': 'uzair',
+        'age': '18',
+        'nationality': 'pakistan',
+        
+    }
+    return render(request,'index.html', contex)
+
+def index2(request):
+    
+    
+    return render(request,'index2.html')
+
+def counter(request):
+    word = request.GET['word']
+    ammount_of_word = len(word.split())
+    return render(request,'counter.html',{'ammount': ammount_of_word})
