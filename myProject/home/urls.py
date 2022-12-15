1c5cb6ac1805d637be986989fcc871e7dc0401e9
sardar uzair <sardaruzair123456789@gmail.com>
2022-12-15 22:30:12 +0500

    compleate with some project and idea

--- myProject/home/urls.py
+++ myProject/home/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('word/', views.index2, name='index2'),
-    path('counter/', views.counter, name='counter')
+    path('', views.index, name='index')
 ]
